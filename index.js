d27f3a40abd7071c55606404d629bf952d84d0f7
ibrahim-777 <ibrahim.karra@gmail.com>
2023-07-03 11:51:30 +0300

    update file

--- index.js
+++ index.js
@@ -1,52 +1,40 @@
-
-// This is how JS will call an API and Print in the console the result
-//  this is how JS REQUESTs a certain API / URL to retrun
-let countries = document.getElementById('countries');
-            let AllCountries = document.getElementById('AllCountries');
-            let flags = document.getElementById('flags');
-            let div = document.createElement('div')
+let flags = document.getElementById('flags');
+let div = document.createElement('div')
             
-fetch('https://restcountries.com/v3.1/all') 
+    fetch('https://restcountries.com/v3.1/all') 
         .then(response => response.json())
         .then(json => {          
             json.forEach(country => {
-               
-                // let option = document.createElement('option');
-                // option.innerHTML = country.name.common;
-                // AllCountries.appendChild(option);
                 let container = document.createElement('div')
-                container.classList.add('card') 
-                
-                let MyImg = document.createElement('img');
-                container.appendChild(MyImg)
-                MyImg.src= country.flags.png;
-                MyImg.id= country.name.common;
-                MyImg.classList.add('img')
+                container.innerHTML = 
+                `<div class="card-group">
+                <div class="card card-columns" style="height: 14rem;">
+    <img class="card-img-top border-primary img" id="${country.name.common}" src="${country.flags.png}" alt="Card image cap"/>
+    </div>
+    </div>
+    `
                  div.classList.add('div22') 
-              
                 //Attach click event on the image , addEventListener
-
                 flags.appendChild(container);
                 container.addEventListener('click',(e)=>{
                     div.innerHTML = `
                     <img class="cancel" id="cancel" onClick={cancelDiv()} src="cancel.png"/>
                     <img class="littleImg" src='${country.flags.png}'/>
                     <h3 style="left:15px;">Name: ${country.name.common}</h3>
-                    <h4>Capital: ${country.capital}</h4> 
-                    <h4>Continents: ${country.continents
-                    }</h4>
-                    `
-                    div.classList.add('show')
-                    
+                    <h5>Capital: ${country.capital}</h5> 
+                    <h5>Continents: ${country.continents}</h5>`
+                    div.classList.add('show')   
                 }) 
           
-            });
+            })
             console.log(json)
-            
-        })
+        });
+
+
         flags.appendChild(div)
              console.log(cancel)
              function cancelDiv(){
                      div.classList.remove('show') 
              }
      
+
